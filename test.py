c29463072c0191008b75a5994275e510b4ffeaf4
Yunzhi Gao <gaoyunzhi@gmail.com>
2020-07-23 11:52:04 -0700

    commit

--- test.py
+++ test.py
@@ -11,12 +11,11 @@ with open('CREDENTIALS') as f:
 	CREDENTIALS = yaml.load(f, Loader=yaml.FullLoader)
 tele = Updater(CREDENTIALS['bot_token'], use_context=True)
 chat = tele.bot.get_chat(-1001198682178)
-channel = tele.bot.get_chat('@web_record')
+# channel = tele.bot.get_chat('@web_record')
 
 def test(url, rotate=False):
 	r = weibo_2_album.get(url)
-	print(r.hash)
 	album_sender.send(chat, url, r, rotate = rotate)
 	
 if __name__=='__main__':
-	test('https://m.weibo.cn/status/J6jAg3KDa')
+	test('https://m.weibo.cn/detail/4529250478338718#comment')
